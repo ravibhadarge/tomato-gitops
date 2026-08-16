2c0dd9b28ef43f517484fe988dcfc9adbaa6e860
Ravi Bhadarge <YOUR_GITHUB_EMAIL>
2026-08-16 15:10:26 +0530

    Fix infra folder and add .gitignore for terraform and secrets

---
+++ infra/envs/dev/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "zen-pharma-terraform-state-ravibhadarge"
+
+    # Replace with your S3 bucket name
+    key          = "envs/dev/terraform.tfstate"
+    region       = "us-east-1"
+#    encrypt      = true
+    use_lockfile = true # S3 native locking
+  }
+}
